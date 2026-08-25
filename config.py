ad9f06e975b18093114a468f65e9045707ea063c
javierusvg <javierusvg@gmail.com>
2026-08-25 12:25:10 +0200

    Cargar y esocoger imagenes

---
+++ config.py
@@ -0,0 +1,10 @@
+# ImageRandomizerFA — Configuración central
+
+MAXIMO_IMAGENES_ALEATORIAS = 6
+
+EXTENSIONES_VALIDAS = {
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".webp",
+}
---
+++ core/ListaImagenes.py
@@ -0,0 +1,26 @@
+import random
+from pathlib import Path
+from config import *
+
+def cargarImagenes(carpetas):
+    imagenesPool = []
+    for carpeta in carpetas:
+        if carpeta.exists() and carpeta.is_dir():
+            for imagen in carpeta.iterdir():
+                if imagen.is_file():
+                    if imagen.suffix.lower() in EXTENSIONES_VALIDAS:
+                        imagenesPool.append(imagen)
+    return imagenesPool
+
+def elegirImagenes(numImagenes,imagenesPool):
+    imagenesAMostrar = []
+    if numImagenes <= 0 or numImagenes > MAXIMO_IMAGENES_ALEATORIAS:
+        raise ValueError(
+            f" El numero de imagenes debe estar entre 1 y "
+            f"{MAXIMO_IMAGENES_ALEATORIAS}."
+        )
+
+    if len(imagenesPool) < numImagenes:
+        raise ValueError("No hay suficientes imagenes cargadas en el pool.")
+
+    return random.sample(imagenesPool, numImagenes)
